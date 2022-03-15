50547d76c287afe4269971e4f814f8f4cd62408b
Nidhhoggr <persie.joseph@gmail.com>
2022-03-15 10:38:33 +0000

    Getting chart filters working

--- analysis/dist/js/script.js
+++ analysis/dist/js/script.js
@@ -1,3 +1,5 @@
+const debug = console.log;
+
 window.onload = () => {
 
   function hoverActiveItemWithPointer(e, activeEl, chart) {
@@ -41,11 +43,35 @@ window.onload = () => {
       options: {
           onClick: (e, activeEl, chart) => {
             if (activeEl[0]) {
-              const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
-              // Substitute the appropriate scale IDs
-              const dataX = inc_chart.scales.x.getValueForPixel(canvasPosition.x);
-              const dataY = inc_chart.scales.y.getValueForPixel(canvasPosition.y);
-              console.log(e, canvasPosition, dataX, dataY, activeEl[0]);
+              const {datasetIndex, index} = activeEl[0];
+              const func = (datasetIndex == 2) ? incompatible : compatible;
+              func({el:"#compatible-filter-table", options: {paginationSize: 10}, filter: (data) => {
+                const filtered = [];
+                const key_modes = ["BOTH","MAJOR","MINOR"];
+                return new Promise(r => {
+                  for (i in data) {
+                    const obj = data[i];
+                    switch(datasetIndex) {
+                      case 0:
+                        if(obj.key_mode == key_modes[index] && obj.transposed_by == 0) {
+                          filtered.push(obj);
+                        }
+                        break;
+                      case 1:
+                        if(obj.key_mode == key_modes[index] && obj.transposed_by != 0) {
+                          filtered.push(obj);
+                        }
+                        break;
+                      case 2:
+                        if(obj.key_mode == key_modes[index] && obj.state == "incompatible_but_in_range") {
+                          filtered.push(obj);
+                        }
+                        break;
+                    }
+                  }
+                  r(filtered);
+                });
+              }});
             }
           },
           onHover: hoverActiveItemWithPointer,
@@ -83,7 +109,20 @@ window.onload = () => {
           onHover: hoverActiveItemWithPointer,
           onClick: (e, activeEl) => {
             if(activeEl[0]) {
-              console.log(activeEl[0]);
+              debug(activeEl[0]);
+              const {index} = activeEl[0];
+              incompatible({el:"#incompatible-filter-table", options: {paginationSize: 10}, filter: (data) => {
+                const filtered = [];
+                return new Promise(r => {
+                  for (i in data) {
+                    const obj = data[i];
+                    if (obj.inc_pitch_length == (index + 1)) {
+                      filtered.push(obj);
+                    }
+                  }
+                  r(filtered);
+                });
+              }});
             }
           },
           scales: {
@@ -104,6 +143,9 @@ window.onload = () => {
       }
   });
 
+  compatible({el:"#compatible-table"});
+  incompatible({el: "#incompatible-table"});
+
   function openFolkWikiFromId(e, cell) {
     const fw_link_id = cell._cell.value;
     if (fw_link_id) {
@@ -111,90 +153,106 @@ window.onload = () => {
     }
   }
 
-  fetch("./../datasets/compatible.json")
-  .then(async (response) => {
-     const _response = await response.json();
-     for (i in _response) {
-      const obj = _response[i];
-      if (obj.state.includes("_after_")) {
-        const explored_json = JSON.parse(obj.explored_json);
-        _response[i].transposed_by = explored_json.transposed;
+  function compatible({el, filter, options}) {
+    fetch("./../datasets/compatible.json")
+    .then(async (response) => {
+      const _response = await response.json();
+      for (i in _response) {
+        const obj = _response[i];
+        _response[i].id = `${obj.id}-${obj.song_id}`;
+      }
+      if(filter) {
+        return await filter(_response);
       }
       else {
-        _response[i].transposed_by = 0;
+        return _response;
       }
-      _response[i].id = `${obj.id}-${obj.song_id}`
-     }
-     return _response;
-  })
-  .then(data => {
-    const table1 = new Tabulator("#compatible-table", {
-      data,           //load row data from array
-      layout:"fitColumns",      //fit columns to width of table
-      responsiveLayout:"hide",  //hide columns that dont fit on the table
-      tooltips:true,            //show tool tips on cells
-      addRowPos:"top",          //when adding a new row, add it to the top of the table
-      history:true,             //allow undo and redo actions on the table
-      pagination:"local",       //paginate the data
-      paginationSize:20,         //allow 7 rows per page of data
-      paginationCounter:"rows", //display count of paginated rows in footer
-      movableColumns:true,      //allow column order to be changed
-      resizableRows:true,       //allow row order to be changed
-      initialSort:[             //set the initial sort order of the data
-          {column:"name", dir:"asc"},
-      ],
-      columns:[                 //define the table columns
-          {title:"ID", field:"id"},
-          {title:"Name", field:"name"},
-          //{title:"State", field:"state"},
-          {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId},
-          {title:"Key Mode", field:"key_mode"},
-          {title:"Transposed By", field:"transposed_by"}
-          //{title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
-          //{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
-          //{title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
-          //{title:"Color", field:"col", width:130, editor:"input"},
-          //{title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
-          //{title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
-      ],
+    })
+    .then(data => {
+      const table1 = new Tabulator(el, {
+        data,           //load row data from array
+        layout:"fitColumns",      //fit columns to width of table
+        responsiveLayout:"hide",  //hide columns that dont fit on the table
+        tooltips:true,            //show tool tips on cells
+        addRowPos:"top",          //when adding a new row, add it to the top of the table
+        history:true,             //allow undo and redo actions on the table
+        pagination:"local",       //paginate the data
+        paginationSize:20,         //allow 7 rows per page of data
+        paginationCounter:"rows", //display count of paginated rows in footer
+        movableColumns:true,      //allow column order to be changed
+        resizableRows:true,       //allow row order to be changed
+        initialSort:[             //set the initial sort order of the data
+            {column:"abc_name", dir:"asc"},
+        ],
+        columns:[                 //define the table columns
+            {title:"ID", field:"id"},
+            {title:"Name", field:"abc_name"},
+            //{title:"State", field:"state"},
+            {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId},
+            {title:"Key Mode", field:"key_mode"},
+            {title:"Transposed By", field:"transposed_by"}
+            //{title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
+            //{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
+            //{title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
+            //{title:"Color", field:"col", width:130, editor:"input"},
+            //{title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
+            //{title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
+        ],
+        ...options
+      });
     });
-  });
-  fetch("./../datasets/incompatible.json")
-  .then(response => {
-     return response.json();
-  })
-  .then(data => {
-    const table2 = new Tabulator("#incompatible-table", {
-      data,           //load row data from array
-      layout:"fitColumns",      //fit columns to width of table
-      responsiveLayout:"hide",  //hide columns that dont fit on the table
-      tooltips:true,            //show tool tips on cells
-      addRowPos:"top",          //when adding a new row, add it to the top of the table
-      history:true,             //allow undo and redo actions on the table
-      pagination:"local",       //paginate the data
-      paginationSize:20,         //allow 7 rows per page of data
-      paginationCounter:"rows", //display count of paginated rows in footer
-      movableColumns:true,      //allow column order to be changed
-      resizableRows:true,       //allow row order to be changed
-      initialSort:[             //set the initial sort order of the data
-          {column:"inc_pitch_length", dir:"asc"},
-      ],
-      columns:[                 //define the table columns
-          {title:"ID", field:"id"},
-          {title:"Name", field:"name"},
-          //{title:"State", field:"state"},
-          {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId},
-          //{title:"Note Length", field:"note_sequence_count"},
-          {title:"Incompatible/Distinct Note Count", field: "inc_pitch_length"},
-          {title:"Incompatible/Distinct Pitches", field: "incompatible_pitches"}
-          //{title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
-          //{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
-          //{title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
-          //{title:"Color", field:"col", width:130, editor:"input"},
-          //{title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
-          //{title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
-      ],
-    });
-  });
+  }
 
+  function incompatible({el, filter, options}) {
+    debug("incompatible()", el, options);
+    fetch("./../datasets/incompatible.json")
+    .then(async(response) => {
+       const _response = await response.json();
+       for (i in _response) {
+         const obj = _response[i];
+         debug(obj);
+         _response[i].id = `${obj.id}-${obj.song_id}`
+       }
+       if(filter) {
+         return await filter(_response);
+       }
+       else {
+         return _response;
+       }
+    })
+    .then(data => {
+      const table2 = new Tabulator(el, {
+        data,           //load row data from array
+        layout:"fitColumns",      //fit columns to width of table
+        responsiveLayout:"hide",  //hide columns that dont fit on the table
+        tooltips:true,            //show tool tips on cells
+        addRowPos:"top",          //when adding a new row, add it to the top of the table
+        history:true,             //allow undo and redo actions on the table
+        pagination:"local",       //paginate the data
+        paginationSize:20,         //allow 7 rows per page of data
+        paginationCounter:"rows", //display count of paginated rows in footer
+        movableColumns:true,      //allow column order to be changed
+        resizableRows:true,       //allow row order to be changed
+        initialSort:[             //set the initial sort order of the data
+            {column:"inc_pitch_length", dir:"asc"},
+        ],
+        columns:[                 //define the table columns
+            {title:"ID", field:"id"},
+            {title:"Name", field:"abc_name"},
+            //{title:"State", field:"state"},
+            {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId},
+            //{title:"Note Length", field:"note_sequence_count"},
+            {title:"Incompatible/Distinct Note Count", field: "inc_pitch_length"},
+            {title:"Incompatible/Distinct Pitches", field: "incompatible_pitches"}
+            //{title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
+            //{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
+            //{title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
+            //{title:"Color", field:"col", width:130, editor:"input"},
+            //{title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
+            //{title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
+        ],
+        ...options
+      });
+    });
+  }
 };
