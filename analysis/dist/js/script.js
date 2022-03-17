08dcb5ae57e751d63acc651d5959ffc49e48d1aa
Nidhhoggr <persie.joseph@gmail.com>
2022-03-17 06:45:30 +0000

    Adding more dynamic capability using tabulator

--- analysis/dist/js/script.js
+++ analysis/dist/js/script.js
@@ -20,21 +20,21 @@ window.onload = () => {
             {
               label: 'Compatible',
               data: [6, 33, 117],
-              backgroundColor: 'rgba(249, 212, 92, 0.4)',
+              backgroundColor: 'rgba(136, 191, 77, 0.4)',
               borderColor: 'rgba(0, 0, 0, 1)',
               borderWidth: 1
           },
           {
               label: 'Compatible After Transposition',
               data: [56, 334, 373],
-              backgroundColor: 'rgba(239, 140, 140, 0.4)',
+              backgroundColor: 'rgba(249, 212, 92, 0.4)',
               borderColor: 'rgba(0, 0, 0, 1)',
               borderWidth: 1
           },
           {
               label: 'Incompatible But In Range',
               data: [0, 26, 20],
-              backgroundColor: 'rgba(136, 191, 77, 0.4)',
+              backgroundColor: 'rgba(239, 140, 140, 0.4)',
               borderColor: 'rgba(0, 0, 0, 1)',
               borderWidth: 1
           }
@@ -45,6 +45,7 @@ window.onload = () => {
             if (activeEl[0]) {
               const {datasetIndex, index} = activeEl[0];
               const func = (datasetIndex == 2) ? incompatible : compatible;
+              document.querySelector(".compatible-filter-item").setAttribute("style","display: block"); 
               func({el:"#compatible-filter-table", options: {paginationSize: 10}, filter: (data) => {
                 const filtered = [];
                 const key_modes = ["BOTH","MAJOR","MINOR"];
@@ -109,9 +110,9 @@ window.onload = () => {
           onHover: hoverActiveItemWithPointer,
           onClick: (e, activeEl) => {
             if(activeEl[0]) {
-              debug(activeEl[0]);
               const {index} = activeEl[0];
-              incompatible({el:"#incompatible-filter-table", options: {paginationSize: 10}, filter: (data) => {
+              document.querySelector(".incompatible-filter-item").setAttribute("style","display: block");
+              incompatible({el:"#incompatible-filter-table", options: {paginationSize: 10, hide_inc_pitch_length: true}, filter: (data) => {
                 const filtered = [];
                 return new Promise(r => {
                   for (i in data) {
@@ -152,7 +153,107 @@ window.onload = () => {
       window.open(`http://www.folkwiki.se/Musik/${fw_link_id}`);
     }
   }
+ 
+  function openFolktabsFromCompatibilityId(e, cell) {
+    const composite_key = cell._cell.value;
+    const id = composite_key.split("-")[0]; 
+    window.open(`https://sackpipa.folktabs.com/?fn=sp-${id}`);
+  }
 
+  function openTransposedAbcFile(e, cell) {
+    const composite_key = cell._cell.row.cells[0].value;
+    const id = composite_key.split("-")[0];
+    window.open(`https://compatibility.folktabs.com/abc/folkwiki/sp-${id}.abc`);
+  }
+
+  const headerTooltips = {
+    "id": "The composite key of the compatibility record to the song by the keymode. Click this cell to open the song in the FolkTabs Sackpipa player.",
+    "fw_link_id": "The FolkWiki.se page of the song. Click this cell to open the in FolkWiki.se",
+    "key_mode": "Whether or not the scallop needs to be plugged. Minor requires plugging the middle finger scallop whereas Major requires unplugging. If the song is Both, than the scallop must be unplugged but requires covering the top hole of the scallop for the required notes.",
+    "inc_pitch_length": "How many disctict incompatible notes occur in the song.",
+    "incompatible_pitches": "The particular pitches that are incompatible which occur in the song",
+    "transposed_by": "How many steps the song had to be transposed in order to become compatible with the chanter. Click this cell to view the modified transposed ABC file."
+  }
+
+  function headerTooltip(column) {
+    const field = column.getDefinition().field;
+    const desc = headerTooltips[field];
+    debug(field, desc);
+    return desc;
+  }
+
+  const defaultTabulatorOpts = {
+    layout:"fitColumns",      //fit columns to width of table
+    responsiveLayout:"hide",  //hide columns that dont fit on the table
+    addRowPos:"top",          //when adding a new row, add it to the top of the table
+    history:false,             //allow undo and redo actions on the table
+    selectable: false,
+    pagination:"local",       //paginate the data
+    paginationSize:20,         //allow 7 rows per page of data
+    paginationCounter:"rows", //display count of paginated rows in footer
+    movableColumns:true,      //allow column order to be changed
+    resizableRows:true,       //allow row order to be changed 
+    columnDefaults:{
+      tooltip:function(cell){
+        const field = cell.getColumn().getField();
+        switch(field) {
+          case "id": 
+            return "Open In Sackpipa Player";
+          case "fw_link_id":
+            return "Open In FolkWiki.se";
+          case "transposed_by":
+            return "Open Transposed ABC File";
+          case "abc_name":
+            return cell.getValue();
+          case "incompatible_pitches":
+            return getNotesFromPitches(cell.getValue());
+        }
+      },
+    }
+  }
+
+  function getNotesFromPitches(p) {
+    const pitches = p.split(",");
+    const mapping = pitches.map((p) => pitchToNoteName[parseInt(p)]);
+    if (mapping.length) {
+      return mapping.join(",");
+    }
+  }
+
+  function setCellCursor(e, f) {
+    const {field} = f._cell.column;
+    if (["fw_link_id","id","transposed_by","incompatible_pitches"].includes(field)) {
+      e.target.style.cursor = "pointer";
+    }
+  }
+
+  function loadTooltips() {
+    setTimeout(() => {
+      const tcs = document.querySelectorAll(".tabulator-cell, [role=columnheader]");
+      let i;
+      for (i in tcs) {
+        const el = tcs[i];
+        if(!el.getAttribute) continue;
+        const title = el.getAttribute('title');
+        if (!title) continue;
+        if (title.trim() !== "" && title.trim() !== "null") {
+          el.setAttribute("data-tippy-content",title);
+        }
+        tcs[i].removeAttribute("title");
+      }
+      tippy("[data-tippy-content]");
+    });
+  }
+
+  function registerTabulatorEvents(table) {
+    table.on("cellMouseEnter", setCellCursor);
+    table.on("cellMouseMove", setCellCursor);
+    table.on("renderStarted", loadTooltips);
+    table.on("renderComplete", loadTooltips);
+    table.on("dataFiltered", loadTooltips);
+    table.on("dataSorted", loadTooltips);
+  };
+    
   function compatible({el, filter, options}) {
     fetch("./../datasets/compatible.json")
     .then(async (response) => {
@@ -171,26 +272,17 @@ window.onload = () => {
     .then(data => {
       const table1 = new Tabulator(el, {
         data,           //load row data from array
-        layout:"fitColumns",      //fit columns to width of table
-        responsiveLayout:"hide",  //hide columns that dont fit on the table
-        tooltips:true,            //show tool tips on cells
-        addRowPos:"top",          //when adding a new row, add it to the top of the table
-        history:true,             //allow undo and redo actions on the table
-        pagination:"local",       //paginate the data
-        paginationSize:20,         //allow 7 rows per page of data
-        paginationCounter:"rows", //display count of paginated rows in footer
-        movableColumns:true,      //allow column order to be changed
-        resizableRows:true,       //allow row order to be changed
+        ...defaultTabulatorOpts,
         initialSort:[             //set the initial sort order of the data
             {column:"abc_name", dir:"asc"},
         ],
         columns:[                 //define the table columns
-            {title:"ID", field:"id"},
-            {title:"Name", field:"abc_name"},
+            {title:"ID", field:"id", cellClick: openFolktabsFromCompatibilityId, headerTooltip},
+            {title:"Name", field:"abc_name", headerFilter: true},
             //{title:"State", field:"state"},
-            {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId},
-            {title:"Key Mode", field:"key_mode"},
-            {title:"Transposed By", field:"transposed_by"}
+            {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId, headerTooltip},
+            {title:"Key Mode", field:"key_mode", headerFilter: true, headerTooltip},
+            {title:"Transposed By", field:"transposed_by", cellClick: openTransposedAbcFile, headerTooltip},
             //{title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
             //{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
             //{title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
@@ -200,18 +292,18 @@ window.onload = () => {
         ],
         ...options
       });
+      registerTabulatorEvents(table1);
     });
   }
 
-  function incompatible({el, filter, options}) {
-    debug("incompatible()", el, options);
+  function incompatible({el, filter, options = {}}) {
     fetch("./../datasets/incompatible.json")
     .then(async(response) => {
        const _response = await response.json();
        for (i in _response) {
          const obj = _response[i];
-         debug(obj);
-         _response[i].id = `${obj.id}-${obj.song_id}`
+         _response[i].id = `${obj.id}-${obj.song_id}`;
+         _response[i].incompatible_pitches = obj.incompatible_pitches.replace("{","").replace("}","");
        }
        if(filter) {
          return await filter(_response);
@@ -223,27 +315,18 @@ window.onload = () => {
     .then(data => {
       const table2 = new Tabulator(el, {
         data,           //load row data from array
-        layout:"fitColumns",      //fit columns to width of table
-        responsiveLayout:"hide",  //hide columns that dont fit on the table
-        tooltips:true,            //show tool tips on cells
-        addRowPos:"top",          //when adding a new row, add it to the top of the table
-        history:true,             //allow undo and redo actions on the table
-        pagination:"local",       //paginate the data
-        paginationSize:20,         //allow 7 rows per page of data
-        paginationCounter:"rows", //display count of paginated rows in footer
-        movableColumns:true,      //allow column order to be changed
-        resizableRows:true,       //allow row order to be changed
+        ...defaultTabulatorOpts,
         initialSort:[             //set the initial sort order of the data
             {column:"inc_pitch_length", dir:"asc"},
         ],
         columns:[                 //define the table columns
-            {title:"ID", field:"id"},
-            {title:"Name", field:"abc_name"},
+            {title:"ID", field:"id", openFolktabsFromCompatibilityId, headerTooltip},
+            {title:"Name", field:"abc_name", headerFilter: true},
             //{title:"State", field:"state"},
-            {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId},
+            {title:"FolkWiki", field:"fw_link_id", cellClick: openFolkWikiFromId, headerTooltip},
             //{title:"Note Length", field:"note_sequence_count"},
-            {title:"Incompatible/Distinct Note Count", field: "inc_pitch_length"},
-            {title:"Incompatible/Distinct Pitches", field: "incompatible_pitches"}
+            {title:"Incompatible/Distinct Note Count", field: "inc_pitch_length", headerFilter: true, visible: !options.hide_inc_pitch_length, headerTooltip},
+            {title:"Incompatible/Distinct Pitches", field: "incompatible_pitches", headerFilter: true, headerTooltip}
             //{title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
             //{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
             //{title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
@@ -253,6 +336,9 @@ window.onload = () => {
         ],
         ...options
       });
+      registerTabulatorEvents(table2);
     });
   }
 };
+
+const pitchToNoteName = {21:"A0",22:"Bb0",23:"B0",24:"C1",25:"Db1",26:"D1",27:"Eb1",28:"E1",29:"F1",30:"Gb1",31:"G1",32:"Ab1",33:"A1",34:"Bb1",35:"B1",36:"C2",37:"Db2",38:"D2",39:"Eb2",40:"E2",41:"F2",42:"Gb2",43:"G2",44:"Ab2",45:"A2",46:"Bb2",47:"B2",48:"C3",49:"Db3",50:"D3",51:"Eb3",52:"E3",53:"F3",54:"Gb3",55:"G3",56:"Ab3",57:"A3",58:"Bb3",59:"B3",60:"C4",61:"Db4",62:"D4",63:"Eb4",64:"E4",65:"F4",66:"Gb4",67:"G4",68:"Ab4",69:"A4",70:"Bb4",71:"B4",72:"C5",73:"Db5",74:"D5",75:"Eb5",76:"E5",77:"F5",78:"Gb5",79:"G5",80:"Ab5",81:"A5",82:"Bb5",83:"B5",84:"C6",85:"Db6",86:"D6",87:"Eb6",88:"E6",89:"F6",90:"Gb6",91:"G6",92:"Ab6",93:"A6",94:"Bb6",95:"B6",96:"C7",97:"Db7",98:"D7",99:"Eb7",100:"E7",101:"F7",102:"Gb7",103:"G7",104:"Ab7",105:"A7",106:"Bb7",107:"B7",108:"C8",109:"Db8",110:"D8",111:"Eb8",112:"E8",113:"F8",114:"Gb8",115:"G8",116:"Ab8",117:"A8",118:"Bb8",119:"B8",120:"C9",121:"Db9"};
